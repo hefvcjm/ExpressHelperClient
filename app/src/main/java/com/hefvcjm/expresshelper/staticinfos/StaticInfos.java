979b95d5f856b90fe915cc2cf34aca1bfcb173cd
hefvcjm <1020657447@qq.com>
2018-06-24 13:41:13 +0800

    添加用户静态信息

---
+++ app/src/main/java/com/hefvcjm/expresshelper/staticinfos/StaticInfos.java
@@ -0,0 +1,15 @@
+package com.hefvcjm.expresshelper.staticinfos;
+
+public class StaticInfos {
+
+    private static String PHONE;
+
+    public static void setPhone(String phone) {
+        PHONE = phone;
+    }
+
+    public static String getPhone() {
+        return PHONE;
+    }
+
+}
